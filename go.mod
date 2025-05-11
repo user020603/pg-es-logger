e261e1545f5dce184b242bfe531c08a9a24516b4
user020603 <ptit.tienthanh@gmail.com>
2025-05-11 23:55:48 +0700

    feat(pkg): create zaplogger config

---
+++ go.mod
@@ -0,0 +1,7 @@
+module thanhnt208/pg-cdc-es
+
+go 1.21.6
+
+require go.uber.org/zap v1.27.0
+
+require go.uber.org/multierr v1.10.0 // indirect
---
+++ pkg/logger/zap.go
@@ -0,0 +1,111 @@
+package logger
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type ILogger interface {
+	Debug(msg string, keysAndValues ...interface{})
+	Info(msg string, keysAndValues ...interface{})
+	Warn(msg string, keysAndValues ...interface{})
+	Error(msg string, keysAndValues ...interface{})
+	Fatal(msg string, keysAndValues ...interface{})
+	Sync() error
+}
+
+var (
+	instance ILogger
+	once     sync.Once
+)
+
+type Logger struct {
+	zap *zap.SugaredLogger
+}
+
+func NewLogger(level string) (ILogger, error) {
+	var initErr error
+	once.Do(func() {
+		encoderConfig := zap.NewProductionEncoderConfig()
+		encoderConfig.TimeKey = "timestamp"
+		encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+		var zapLevel zapcore.Level
+		if err := zapLevel.Set(level); err != nil {
+			zapLevel = zapcore.InfoLevel
+		}
+
+		if err := os.MkdirAll("log", 0755); err != nil {
+			initErr = fmt.Errorf("failed to create log directory: %w", err)
+			return
+		}
+
+		logFile, err := os.OpenFile("log/log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			initErr = fmt.Errorf("failed to open log file: %w", err)
+			return
+		}
+
+		core := zapcore.NewTee(
+			zapcore.NewCore(
+				zapcore.NewJSONEncoder(encoderConfig),
+				zapcore.AddSync(logFile),
+				zapLevel,
+			),
+			zapcore.NewCore(
+				zapcore.NewConsoleEncoder(encoderConfig),
+				zapcore.AddSync(os.Stdout),
+				zapLevel,
+			),
+		)
+
+		logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+		instance = &Logger{
+			zap: logger.Sugar(),
+		}
+	})
+
+	if initErr != nil {
+		return nil, initErr
+	}
+
+	if instance == nil {
+		return nil, errors.New("logger instance is nil")
+	}
+
+	return instance, nil
+}
+
+func addLevelPrefix(level, msg string) string {
+	return fmt.Sprintf("[%s] %s", strings.ToUpper(level), msg)
+}
+
+func (l *Logger) Debug(msg string, keysAndValues ...interface{}) {
+	l.zap.Debugw(addLevelPrefix("DEBUG", msg), keysAndValues...)
+}
+
+func (l *Logger) Info(msg string, keysAndValues ...interface{}) {
+	l.zap.Infow(addLevelPrefix("INFO", msg), keysAndValues...)
+}
+
+func (l *Logger) Warn(msg string, keysAndValues ...interface{}) {
+	l.zap.Warnw(addLevelPrefix("WARN", msg), keysAndValues...)
+}
+
+func (l *Logger) Error(msg string, keysAndValues ...interface{}) {
+	l.zap.Errorw(addLevelPrefix("ERROR", msg), keysAndValues...)
+}
+
+func (l *Logger) Fatal(msg string, keysAndValues ...interface{}) {
+	l.zap.Fatalw(addLevelPrefix("FATAL", msg), keysAndValues...)
+}
+
+func (l *Logger) Sync() error {
+	return l.zap.Sync()
+}
